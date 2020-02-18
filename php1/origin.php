fe4a0d1891032926bfad2f78d87c0f8659384bb2
mattmatowy <44133388+mattmatowy@users.noreply.github.com>
2020-02-18 13:29:46 +0100

    Add files via upload
    php lesson 1

---
+++ php1/origin.php
@@ -0,0 +1,24 @@
+<!DOCKTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+
+</head>
+
+<body>
+
+<?php
+
+$imie = "Eva";
+
+echo "$imie witaj na stronie!";
+
+echo '<img src="evagreen.jpg"/>';
+ 
+
+?>	
+	
+
+</body>
+
+</html>
